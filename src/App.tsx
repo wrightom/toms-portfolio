c452862b1d4d5403cbeb6fc19734f36a8bebf75c
Tom Wright <tomnwright@outlook.com>
2025-09-05 17:49:59 +0100

    Dynamic title

--- src/App.tsx
+++ src/App.tsx
@@ -98,105 +98,107 @@ function Project({ project }: { project: ProjectData, activeDefault?: boolean })
   return (
     <a className="relative group aspect-square rounded-xl overflow-hidden ring-1 ring-black/10" ref={selfRef} href={project.link} title={project.linkDescr} target="_blank" rel="noopener noreferrer" onMouseMove={onMouseOver} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} onClick={handleClick}>
       <img src={project.imgUrl || "https://picsum.photos/500/200"} alt="" className="w-full h-full object-cover" />
-      <div className={cat("title-bar text-white min-h-15 w-full absolute flex justify-between items-center gap-1 bg-[var(--theme-blue)] backdrop-blur-xs px-3 py-1 top-full transition-all duration-500",(active || activating)?"-translate-y-full":"")}>
+      <div className={cat("title-bar text-white min-h-15 w-full absolute flex justify-between items-center gap-1 bg-[var(--theme-blue)] backdrop-blur-xs px-3 py-1 top-full transition-all duration-500", (active || activating) ? "-translate-y-full" : "")}>
         <p>{project.name}</p>
         <div><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={`h-6 w-6 transition-all delay-200 duration-400 ${active ? "opacity-100" : "-translate-x-2 translate-y-2 opacity-0"}`}><path d="M7 7h10v10"></path><path d="M7 17 17 7"></path></svg></div>
       </div>
     </a >
   )
-      // return (
+  // return (
 
-      //   <a ref={selfRef} href={project.link} title={project.linkDescr} target="_blank" rel="noopener noreferrer" className={cat("group title-container relative",
-      //   ) + (active || activating ? " active" : "")} onMouseMove={onMouseOver} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} onClick={handleClick}>
-      //     <div className="flex gap-5 items-center">
-      //       <h2 className={"title smooth"}>{project.name}</h2>
-      
-      //     </div>
-      //     {/* <div className={"absolute inset-1 -z-1 bg-gray-100/40 rounded-md transition-all delay-0 group-hover:delay-300 duration-700 " + (active ? "opacity-0" : "opacity-100")}></div> */}
+  //   <a ref={selfRef} href={project.link} title={project.linkDescr} target="_blank" rel="noopener noreferrer" className={cat("group title-container relative",
+  //   ) + (active || activating ? " active" : "")} onMouseMove={onMouseOver} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} onClick={handleClick}>
+  //     <div className="flex gap-5 items-center">
+  //       <h2 className={"title smooth"}>{project.name}</h2>
 
-      //     <div className={cat(
-      //       active ? 'max-h-[250px]' : 'max-h-0',
-      //       active ? "opacity-100" : "opacity-0",
-      //       "overflow-hidden",
-      //       "transition-all delay-0 group-hover:delay-300 duration-700",
-      //     )}>
-      //       <div className="tags-container flex gap-2 my-2 flex-wrap">
-      //         {project.tags?.map((tag: string, index: number) => (
-      //           <Tag name={tag} key={index} />
-      //         ))}
-      //       </div>
-      //       <p>{project.descr}</p>
-      //     </div>
-      //     <div className={cat(
-      //       "ring-black/10",
-      //       // active ? "ring-black/10 shadow-lg" : "ring-transparent",
-      //       // active ? "opacity-100" : "opacity-0",
-      //       "pointer-events-none",
-      //       "absolute -z-5",
-      //       active ? "-inset-4" : "-inset-2",
-      //       "bg-white/80 backdrop-blur-[2px]",
-      //       "ring-1 rounded-xl",
-      //       "transition-all delay-0 group-hover:delay-300 duration-700",
-      //     )}></div>
+  //     </div>
+  //     {/* <div className={"absolute inset-1 -z-1 bg-gray-100/40 rounded-md transition-all delay-0 group-hover:delay-300 duration-700 " + (active ? "opacity-0" : "opacity-100")}></div> */}
 
-      //   </a >)
-    }
+  //     <div className={cat(
+  //       active ? 'max-h-[250px]' : 'max-h-0',
+  //       active ? "opacity-100" : "opacity-0",
+  //       "overflow-hidden",
+  //       "transition-all delay-0 group-hover:delay-300 duration-700",
+  //     )}>
+  //       <div className="tags-container flex gap-2 my-2 flex-wrap">
+  //         {project.tags?.map((tag: string, index: number) => (
+  //           <Tag name={tag} key={index} />
+  //         ))}
+  //       </div>
+  //       <p>{project.descr}</p>
+  //     </div>
+  //     <div className={cat(
+  //       "ring-black/10",
+  //       // active ? "ring-black/10 shadow-lg" : "ring-transparent",
+  //       // active ? "opacity-100" : "opacity-0",
+  //       "pointer-events-none",
+  //       "absolute -z-5",
+  //       active ? "-inset-4" : "-inset-2",
+  //       "bg-white/80 backdrop-blur-[2px]",
+  //       "ring-1 rounded-xl",
+  //       "transition-all delay-0 group-hover:delay-300 duration-700",
+  //     )}></div>
 
-
+  //   </a >)
+}
 
 
-      function App() {
-
-    return (
-      // Tailwind: centers the container horizontally (mx-auto), sets different maximum widths for various screen sizes (max-w-2xl, sm:max-w-3xl, md:max-w-4xl), and adds top and bottom padding that increases on medium screens (pt-28 pb-20 md:pt-32)
-      <div className="content mx-auto max-w-2xl pt-28 pb-20 sm:max-w-3xl md:max-w-4xl md:pt-32 px-6 flex flex-col gap-3">
-        {/* <a href="https://tomwright.io"> */}
-        <h1 className="-ml-1 title smooth hover-active"><div className="absolute -inset-5"></div>Tom Wright</h1>
-        <LinkIcons className="mb-15" />
-        {/* </a> */}
-          <p>Hi, I'm <Link href="https://tomwright.io">Tom</Link>. Welcome to my portfolio. 👋</p>
-        <div className="flex sm:flex-row flex-col gap-4 items-stretch">
-       
-            <Bullet>
-              I'm passionate about building innovative tech and applying cutting-edge techniques to solve problems.
-            </Bullet>
-            <Bullet>
-              I graduated in 2023 from the University of Birmingham with a First-Class BSc Mathematics and an overall score of 81%.
-            </Bullet>
-            <Bullet>
-              The companies I have worked for include
-              {" "}<Link href="https://fiecon.com" newtab>FIECON</Link>,
-              {" "}<Link href="https://ample.earth" newtab>Ample</Link>, and
-              {" "}<Link href="https://ricardo.com" newtab>Ricardo</Link>.
-            </Bullet>
-
-        </div>
-        <div className="projects-title mt-15">
-          <div className="relative flex py-5 items-center">
-            <div className="border-t w-5 border-[var(--theme-blue)]"></div>
-            <span className="flex-shrink mx-4 text-gray-700">Projects</span>
-            <div className="flex-grow border-t border-[var(--theme-blue)]"></div>
-          </div>
-          <p>Click on the links below to find out more about what I've built.</p>
-        </div>
 
 
-        <div id="projects-container" className="grid sm:grid-cols-5 grid-cols-3 gap-2">
-          {projects.map((project: ProjectData, index: number) => (
-            <Project project={project} key={index} />
-          ))}
-        </div>
+function App() {
 
-
-        <div className="mt-10"></div>
+  return (
+    // Tailwind: centers the container horizontally (mx-auto), sets different maximum widths for various screen sizes (max-w-2xl, sm:max-w-3xl, md:max-w-4xl), and adds top and bottom padding that increases on medium screens (pt-28 pb-20 md:pt-32)
+    <div className="content mx-auto max-w-2xl pt-28 pb-20 sm:max-w-3xl md:max-w-4xl md:pt-15 px-6 flex flex-col gap-3">
+      {/* <a href="https://tomwright.io"> */}
+      <div className="hover-parent mb-10 pt-15">
+        <h1 className="-ml-1 mb-2 title smooth child-active">Tom Wright</h1>
+        <LinkIcons className="mb-5"/>
       </div>
-    );
-  }
+      {/* </a> */}
+      <p>Hi, I'm <Link href="https://tomwright.io">Tom</Link>. Welcome to my portfolio. 👋</p>
+      <div className="flex sm:flex-row flex-col gap-4 items-stretch">
 
-  export default App;
+        <Bullet>
+          I'm passionate about building innovative tech and applying cutting-edge techniques to solve problems.
+        </Bullet>
+        <Bullet>
+          I graduated in 2023 from the University of Birmingham with a First-Class BSc Mathematics and an overall score of 81%.
+        </Bullet>
+        <Bullet>
+          The companies I have worked for include
+          {" "}<Link href="https://fiecon.com" newtab>FIECON</Link>,
+          {" "}<Link href="https://ample.earth" newtab>Ample</Link>, and
+          {" "}<Link href="https://ricardo.com" newtab>Ricardo</Link>.
+        </Bullet>
+
+      </div>
+      <div className="projects-title mt-15">
+        <div className="relative flex py-5 items-center">
+          <div className="border-t w-5 border-[var(--theme-blue)]"></div>
+          <span className="flex-shrink mx-4 text-gray-700">Projects</span>
+          <div className="flex-grow border-t border-[var(--theme-blue)]"></div>
+        </div>
+        <p>Click on the links below to find out more about what I've built.</p>
+      </div>
 
 
-function Bullet({children}:{children:React.ReactNode}){
+      <div id="projects-container" className="grid sm:grid-cols-5 grid-cols-3 gap-2">
+        {projects.map((project: ProjectData, index: number) => (
+          <Project project={project} key={index} />
+        ))}
+      </div>
+
+
+      <div className="mt-10"></div>
+    </div>
+  );
+}
+
+export default App;
+
+
+function Bullet({ children }: { children: React.ReactNode }) {
   return (
     <div className="smooth anim-card ring-1 ring-[var(--theme-blue)] p-5 rounded-xl">{children}</div>
   )
